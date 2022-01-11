0023e4e990ffee14361358275d185e3b959268e7
Lizbeth <lizbethgg07@gmail.com>
2022-01-11 02:19:51 -0600

    Separemos rutas y controladores

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.rutasUsuarios = "/usuarios";
 
+        this.middlewares();
         this.routes();
     }
 
+    middlewares() {
+        this.app.use(cors());
+    }
+
     routes() {
-        this.app.get('/', function(req, res) {
-            res.send('Hola a todos');
-        });
+        this.app.use(this.rutasUsuarios, require("./routes/usuarios"));
     }
 
     listen() {
-        this.app.listen(process.env.PORT);
+        this.app.listen(this.port);
     }
 }
 
